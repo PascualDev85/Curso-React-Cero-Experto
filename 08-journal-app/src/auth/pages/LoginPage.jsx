8a1ca44e7755873113027611269f8ac6074a98eb
David <pascual.dev85@gmail.com>
2022-11-24 12:01:57 +0100

    TODO auth firebase

--- 08-journal-app/src/auth/pages/LoginPage.jsx
+++ 08-journal-app/src/auth/pages/LoginPage.jsx
@@ -1,12 +1,37 @@
+import { useDispatch } from "react-redux";
 import { Link as RouterLink } from "react-router-dom";
 import { Grid, Typography, TextField, Button, Link } from "@mui/material";
 import Google from "@mui/icons-material/Google";
+
 import { AuthLayout } from "../layout/AuthLayout";
 
+import { useForm } from "../../hooks";
+import { checkingAuthentication, startGoogleSignIn } from "../../store/auth";
+
 export const LoginPage = () => {
+  const dispatch = useDispatch();
+  const { email, password, onInputChange } = useForm({
+    email: "dpmeta@gmail.com",
+    password: "123456",
+  });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    //console.log({ email, password });
+
+    dispatch(checkingAuthentication());
+  };
+
+  const onGoogleSignIn = () => {
+    console.log("Google Sign In");
+
+    dispatch(startGoogleSignIn());
+  };
+
   return (
     <AuthLayout title="Login">
-      <form>
+      <form onSubmit={onSubmit}>
         <Grid container>
           <Grid item xs={12} sx={{ mt: 2 }}>
             <TextField
@@ -14,6 +39,9 @@ export const LoginPage = () => {
               type="email"
               placeholder="email@gmail.com"
               fullWidth
+              name="email"
+              value={email}
+              onChange={onInputChange}
             />
           </Grid>
           <Grid item xs={12} sx={{ mt: 2 }}>
@@ -22,16 +50,29 @@ export const LoginPage = () => {
               type="password"
               placeholder="Password"
               fullWidth
+              name="password"
+              value={password}
+              onChange={onInputChange}
             />
           </Grid>
           <Grid container spacing={2} sx={{ mb: 2, mt: 2 }}>
             <Grid item xs={12} sm={6}>
-              <Button variant="contained" sx={{ padding: 2 }} fullWidth>
+              <Button
+                type="submit"
+                variant="contained"
+                sx={{ padding: 2 }}
+                fullWidth
+              >
                 <Typography>Login</Typography>
               </Button>
             </Grid>
             <Grid item xs={12} sm={6}>
-              <Button variant="contained" sx={{ padding: 2 }} fullWidth>
+              <Button
+                onClick={onGoogleSignIn}
+                variant="contained"
+                sx={{ padding: 2 }}
+                fullWidth
+              >
                 <Google />
                 <Typography sx={{ ml: 1 }}>Google</Typography>
               </Button>
---
+++ 08-journal-app/src/store/auth/thunks.js
@@ -0,0 +1,15 @@
+// acciones que puedo hacer dispatch que son funciones asincronas
+
+import { checkingCredentials } from "./";
+
+export const checkingAuthentication = (email, password) => {
+  return async (dispatch) => {
+    dispatch(checkingCredentials());
+  };
+};
+
+export const startGoogleSignIn = () => {
+  return async (dispatch) => {
+    dispatch(checkingCredentials());
+  };
+};
