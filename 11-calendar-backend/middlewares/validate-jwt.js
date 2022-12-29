e64d1de0cf6f89e83ad410fcd26e88bdabfa8abe
David <pascual.dev85@gmail.com>
2022-12-29 20:30:09 +0100

    TODO Section 23

---
+++ 11-calendar-backend/middlewares/validate-jwt.js
@@ -0,0 +1,31 @@
+const { response } = require("express");
+
+const validarJWT = (req, res = response, next) => {
+  // Leer el token
+  const token = req.header("x-token");
+
+  if (!token) {
+    return res.status(401).json({
+      ok: false,
+      msg: "No hay token en la petición",
+    });
+  }
+
+  try {
+    const { uid, name } = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.uid = uid;
+    req.name = name;
+  } catch (error) {
+    return res.status(401).json({
+      ok: false,
+      msg: "Token no válido",
+    });
+  }
+
+  next();
+};
+
+module.exports = {
+  validarJWT,
+};
